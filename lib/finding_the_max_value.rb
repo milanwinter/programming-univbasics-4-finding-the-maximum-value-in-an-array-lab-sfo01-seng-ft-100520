4b6200b0c4da3fefccfbf3aaf129f702dc6ff5e2
Milan Winter <milanwinter8@gmail.com>
2020-09-23 20:49:23 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  # Add your solution here
+  comparison = 0
+  count = 0
+  while count < array.length
+    if array[count] > comparison
+      comparison = array[count]
+    end
+    count += 1
+  end
+  comparison
 end
